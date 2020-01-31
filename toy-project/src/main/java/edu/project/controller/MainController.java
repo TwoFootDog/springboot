559e441cf00fd64a651a66cb73957b6d9342142d
강민규(KANG MIN KYU)/통신/미디어Digital추진그룹/SK <08314@skcc.com>
2020-01-31 18:31:14 +0900

    cc

--- toy-project/src/main/java/edu/project/controller/MainController.java
+++ toy-project/src/main/java/edu/project/controller/MainController.java
@@ -1,13 +1,22 @@
 package edu.project.controller;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
+@Log4j
 public class MainController {
+
+    private Logger logger = LoggerFactory.getLogger(MainController.class);
 
     @GetMapping("/")
     public String rootFunction() {
+        logger.info("--------------SUCCESS-----------");
         return "SUCCESS";
     }
 }
